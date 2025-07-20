e28cf81fc306a745ac959ff62e182020d14a7485
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-07-20 18:54:21 +0300

    fix: installing

--- build.zig
+++ build.zig
@@ -5,6 +5,7 @@ pub fn build(b: *std.Build) void {
     var j = Janitor.init(b);
     j.exe("chess-engine");
     j.clib("castro", "castro/src/castro.h", "castro/src/", "castro/libcastro.a");
+    j.install();
 
     j.step(.clean);
     j.step(.run);
