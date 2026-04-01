78731bbce51de11449bd5d220e94d16ac77fab45
Konstantinos Despoinidis <kdesp2003@gmail.com>
2026-04-01 18:15:29 +0300

    feat: added updated castro libs

--- make/dist.mk
+++ make/dist.mk
@@ -1,4 +1,4 @@
-DIST_INCLUDE = $(INCLUDE_DIR) $(SO_NAME) $(A_NAME) $(TARGET) data deps
+DIST_INCLUDE = $(INCLUDE_DIR) $(SO_NAME) $(A_NAME) $(TARGET) deps apps/
 
 .PHONY: dist
 dist: build.all ## Create a tarball of the project
